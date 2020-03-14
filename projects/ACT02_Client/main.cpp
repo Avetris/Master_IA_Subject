8165ca124ce5fa7b745c7f544313839b9714ce29
SarkV <aitorvt95@gmail.com>
2020-03-14 18:13:53 +0100

    Thread need to be done
    Game finished. It has to be done the use of threads to multiusers.

--- projects/ACT02_Client/main.cpp
+++ projects/ACT02_Client/main.cpp
@@ -1,3 +1,209 @@
-int main() {
+#include "net/socketutils.h"
+#include "net/socketaddress.h"
+#include <thread>
+#include <vector>
+#include <string>
 
+std::vector<std::string> options;
+
+
+void getOptions(std::string response) {
+    std::string opt = "";
+    uint8_t wordPos = 0;
+    for (char c : response) {
+        if (c == ';' || wordPos == response.size() - 1) {
+            if (wordPos == response.size() - 1) {
+                opt += c;
+            }
+            options.push_back(opt);
+            opt = "";
+        }
+        else {
+            opt += c;
+        }
+        wordPos++;
+    }
 }
+
+void printOptions() {
+    std::cout << "Select an option (write 'exit' to end): " << std::endl;
+    for (int i = 0; i < options.size(); i++) {
+        std::cout << i << " - " << options.at(i) << std::endl;
+    }
+}
+
+std::string checkInput(bool randomInteraction) {
+    std::string input;
+    std::cin >> input;
+    std::string selectedOption;
+    if (randomInteraction) {
+        selectedOption = options.at(rand() % options.size());
+    }
+    else {
+        if (input == "exit") {
+            selectedOption = "exit";
+        }
+        for (int i = 0; i < options.size() && selectedOption.empty(); i++) {
+            if (input == std::to_string(i) || input == options.at(i)) {
+                selectedOption = options.at(i);
+            }
+        }
+        if (selectedOption.empty()) {
+            std::cout << "You have not selected a valid option." << std::endl;
+            printOptions();
+            selectedOption = checkInput(randomInteraction);
+        }
+    }
+    return selectedOption;
+}
+
+void printStageResult(std::string value) {
+    std::string word;
+    uint8_t pos = 0;
+    uint8_t wordPos = 0;
+    for (char c : value) {
+        if (c == '/' || wordPos == value.size() - 1) {
+            if (wordPos == value.size() - 1) {
+                word += c;
+            }
+            if (pos == 0) {
+                if (std::atoi(word.c_str()) == 1) {
+                    std::cout << "You have won the stage!" << std::endl;
+                }
+                else if (std::atoi(word.c_str()) == -1) {
+                    std::cout << "You have lost the stage!" << std::endl;
+                }
+                else {
+                    std::cout << "There has been a draw in the stage!" << std::endl;
+                }
+            }
+            else if (pos == 1) {
+                std::cout << "You: " << word << std::endl;
+            }
+            else if (pos == 2) {
+                std::cout << "Other: " << word << std::endl;
+            }
+            pos++;
+            word = "";
+        }
+        else {
+            word += c;
+        }
+        wordPos++;
+    }
+    std::cout << std::endl << "------------------------------" << std::endl << std::endl;
+}
+
+int checkResponse(int* result, std::string response) {
+    std::string val = "";
+    uint8_t part = 0;
+    uint8_t wordPos = 0;
+    int stage = 0;
+    for (char c : response) {
+        if (c == ';' || wordPos == response.size() - 1) {
+            if (wordPos == response.size() - 1) {
+                val += c;
+            }
+            switch (part)
+            {
+            case 0:
+                stage = std::atoi(val.c_str());
+                break;
+            case 1:
+                printStageResult(val);
+                break;
+            case 2:
+                std::string res = "";
+                uint8_t resPos = 0;
+                uint8_t resWordPos = 0;
+                for (char v : val ) {
+                    if (v == '/' || resWordPos == val.size() - 1) {
+                        if (resWordPos == val.size() - 1) {
+                            res += v;
+                        }
+                        result[resPos] = std::atoi(res.c_str());
+                        resPos++;
+                        res = "";
+                    }
+                    else {
+                        res += v;
+                    }
+                    resWordPos++;
+                }
+                break;
+            }
+            part++;
+            val = "";
+        }
+        else {
+            val += c;
+        }
+        wordPos++;
+    }
+    return stage;
+}
+
+int main() {
+    SocketUtils::init();
+
+    {
+        TCPSocketPtr socket = SocketUtils::createTCPSocket(SocketUtils::INET);
+
+        SocketAddressPtr address = SocketUtils::createIPv4FromString("127.0.0.1:9090");
+
+        socket->connectTo(*address.get());
+
+        constexpr size_t bufferLength = 255;
+        char buffer[bufferLength];
+
+        std::string response;
+
+        int stage = 0;
+        int result[2] = { 0 };
+
+        std::cout << "Do you want to play? (In caso of saying no, there will be a match between machines)" << std::endl;
+        std::string res;
+        while (res._Equal("yes") && !res._Equal("no")) {
+            std::cout << "(yes/no)";
+            std::cin >> res;
+        }
+        bool randomInteraction = res._Equal("no");
+
+        while (!response._Equal("end")) {
+            if (options.size() == 0) {
+                sprintf(buffer, "start");
+                socket->sendTo(buffer, std::string(buffer).size() + 1);
+                socket->receiveFrom(buffer, bufferLength);
+
+                getOptions(std::string(buffer));
+
+                std::cout << "Starting the Game" << std::endl;
+            }
+
+            std::cout << "Stage: " << stage << " - Result: " << result[0] << " - " << result[1] << std::endl;
+
+
+            std::string selectedOption;
+            if (!randomInteraction) {
+                printOptions();
+            }
+            selectedOption = checkInput(randomInteraction);
+
+            sprintf(buffer, selectedOption.c_str());
+            socket->sendTo(buffer, std::string(buffer).size() + 1);
+
+            socket->receiveFrom(buffer, bufferLength);
+            
+            response = std::string(buffer);
+
+            if (!response._Equal("end")) {
+                stage = checkResponse(result, response);
+            }
+
+            //std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+        }
+    }
+
+    SocketUtils::shutdown();
+}
+
--- projects/ACT02_Server/main.cpp
+++ projects/ACT02_Server/main.cpp
@@ -1,3 +1,177 @@
-int main() {
+#include "net/socketutils.h"
+#include "net/socketaddress.h"
+#include <string>
+#include <thread>
+#include <vector>
 
+const std::vector<std::string> options = {
+    "Rock", "Paper", "Scissors", "Lizard", "Spock"
+};
+
+class Game {
+public:
+    void init() {
+
+    }
+
+private:
+    SocketAddress _clientAddress;
+};
+
+std::vector<Game> listGames;
+
+const enum RESULT {
+    WIN = 1,
+    LOSE = -1,
+    DRAW = 0
+};
+
+int chooseWinner(std::string player, std::string decission) {
+    int result = RESULT::DRAW;
+    if (player._Equal(decission)) {
+        result = RESULT::DRAW;
+    }else if (player._Equal(options[0])) {
+        if (decission._Equal(options[1])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[2])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[3])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[4])) {
+            result = RESULT::LOSE;
+        }
+    }
+    else if (player._Equal(options[1])) {
+        if (decission._Equal(options[0])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[2])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[3])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[4])) {
+            result = RESULT::WIN;
+        }
+    }
+    else if (player._Equal(options[2])) {
+        if (decission._Equal(options[1])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[0])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[3])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[4])) {
+            result = RESULT::LOSE;
+        }
+    }
+    else if (player._Equal(options[3])) {
+        if (decission._Equal(options[1])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[2])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[0])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[4])) {
+            result = RESULT::WIN;
+        }
+    }
+    else if (player._Equal(options[4])) {
+        if (decission._Equal(options[1])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[2])) {
+            result = RESULT::WIN;
+        }
+        else if (decission._Equal(options[3])) {
+            result = RESULT::LOSE;
+        }
+        else if (decission._Equal(options[0])) {
+            result = RESULT::WIN;
+        }
+    }
+    return result;
+}
+
+std::string generateResponse(int stage, int result, int* globalResult, std::string player, std::string decission) {
+    std::string returnResult = std::to_string(stage) + ";";
+    returnResult += std::to_string(result) + "/";
+    returnResult += player + "/";
+    returnResult += decission + ";";
+    returnResult += std::to_string(globalResult[0]) + "/";
+    returnResult += std::to_string(globalResult[1]);
+    return returnResult;
+}
+
+int _stage = 0;
+int _result[2] = { 0 };
+
+int main() {
+    SocketUtils::init();
+
+    {
+        TCPSocketPtr socket = SocketUtils::createTCPSocket(SocketUtils::INET);
+
+        SocketAddressPtr address = SocketUtils::createIPv4FromString("127.0.0.1:9090");
+
+        socket->bindTo(*address.get());
+        socket->listenTo();
+
+        SocketAddress addressIn;
+        TCPSocketPtr connSocket = socket->acceptCon(addressIn);
+
+        constexpr size_t bufferLength = 255;
+        char buffer[bufferLength];
+        
+        for (;;) {
+            connSocket->receiveFrom(buffer, bufferLength);
+            std::cout << "Received = " << buffer << std::endl;
+
+            if (std::string(buffer) == "exit") {
+                std::cout << "Exiting = " << buffer << std::endl;
+                break;
+            }
+
+            //std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+            std::string response = std::string(buffer);
+
+            if (response._Equal("start")) {
+                std::string optionsSring = "";
+                for (std::string op : options) {
+                    if (!optionsSring.empty()) {
+                        optionsSring += ";";
+                    }
+                    optionsSring += op;
+                }
+                sprintf(buffer, optionsSring.c_str());
+            }
+            else if (response._Equal("exit")) {
+                sprintf(buffer, "end");
+            }
+            else {
+                std::string result = options.at(rand() % options.size());
+                int winner = chooseWinner(std::string(buffer), result);
+                _stage++;
+                if(winner != 0)
+                    _result[winner == -1 ? 1 : 0] += 1;
+                sprintf(buffer, generateResponse(_stage, winner, _result, response, result).c_str());
+            }
+            connSocket->sendTo(buffer, std::string(buffer).size() + 1);
+
+            //std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+        }
+    }
+
+    SocketUtils::shutdown();
 }
